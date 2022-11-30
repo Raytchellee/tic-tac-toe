82668fe8927b7c9909b74aa62bacaa5a56051191
raytchellee <amakaoforkaja@gmail.com>
2022-11-30 18:51:50 +0100

    modified src/index.css and defined the Square, Board and Game components in src/index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          console.log("Clicked!");
+          this.setState({ value: "X" });
+        }}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+  render() {
+    const status = "Next player: X";
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+          {this.renderSquare(3)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+          {this.renderSquare(6)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+          {this.renderSquare(9)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return <Board />;
+  }
+}
+let root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<Game />);
